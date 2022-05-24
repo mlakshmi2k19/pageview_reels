a830d15066c71575db5d1ab16feaa39a65684293
unknown <mlakshmi2k19@gmail.com>
2022-05-24 20:51:47 +0530

    adding files

---
+++ lib/main.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:video_shorts/videoplayer_remote.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+      ),
+      home: const MyHomePage(title: 'Shorts'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  PageController controller = PageController();
+  static dynamic currentPageValue = 0.0;
+
+  List pageViewItem = [
+    page(
+        'https://archive.org/download/SampleVideo1280x7205mb/SampleVideo_1280x720_5mb.mp4'),
+    page(
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'),
+    page('https://filesamples.com/samples/video/mp4/sample_640x360.mp4'),
+    page('http://techslides.com/demos/sample-videos/small.mp4'),
+    page(
+        'https://archive.org/download/SampleVideo1280x7205mb/SampleVideo_1280x720_5mb.mp4'),
+    page(
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'),
+    page('https://filesamples.com/samples/video/mp4/sample_640x360.mp4'),
+    page('http://techslides.com/demos/sample-videos/small.mp4'),
+    page(
+        'https://archive.org/download/SampleVideo1280x7205mb/SampleVideo_1280x720_5mb.mp4'),
+    page(
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'),
+    page('https://filesamples.com/samples/video/mp4/sample_640x360.mp4'),
+    page('http://techslides.com/demos/sample-videos/small.mp4'),
+    page(
+        'https://archive.org/download/SampleVideo1280x7205mb/SampleVideo_1280x720_5mb.mp4'),
+    page('https://filesamples.com/samples/video/mp4/sample_640x360.mp4'),
+    page('http://techslides.com/demos/sample-videos/small.mp4'),
+    page(
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'),
+    page(
+        'https://archive.org/download/SampleVideo1280x7205mb/SampleVideo_1280x720_5mb.mp4'),
+    page('https://filesamples.com/samples/video/mp4/sample_640x360.mp4'),
+    page('http://techslides.com/demos/sample-videos/small.mp4'),
+    page(
+        'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4'),
+    
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(() {
+      setState(() {
+        currentPageValue = controller.page;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      // ),
+      body: PageView.builder(
+          itemCount: pageViewItem.length,
+          scrollDirection: Axis.vertical,
+          controller: controller,
+          itemBuilder: (context, position) {
+            return Transform(
+              transform: Matrix4.identity()
+                ..rotateX(currentPageValue - position),
+              child: pageViewItem[position],
+            );
+          }),
+    );
+  }
+}
+
+Widget page(String url) {
+  return Container(
+    width: double.infinity,
+    height: double.infinity,
+    child: VideoPlayerRemote(
+      url: url,
+    ),
+  );
+}
---
+++ lib/videoplayer_remote.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerRemote extends StatefulWidget {
+  final String url;
+  const VideoPlayerRemote({required this.url});
+  @override
+  _VideoPlayerRemoteState createState() => _VideoPlayerRemoteState();
+}
+
+class _VideoPlayerRemoteState extends State<VideoPlayerRemote> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.network(
+      widget.url,
+    );
+
+    _controller.addListener(() {
+      setState(() {});
+    });
+    _controller.setLooping(true);
+    _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose(); //dispose the VideoPlayer
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Flexible(
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: AspectRatio(
+                aspectRatio: _controller.value.aspectRatio,
+                child: Stack(
+                  alignment: Alignment.bottomCenter,
+                  children: <Widget>[
+                    VideoPlayer(_controller),
+                    _PlayPauseOverlay(controller: _controller),
+                    VideoProgressIndicator(_controller, allowScrubbing: true),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _PlayPauseOverlay extends StatelessWidget {
+  const _PlayPauseOverlay({Key? key, required this.controller})
+      : super(key: key);
+
+  final VideoPlayerController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        AnimatedSwitcher(
+          duration: const Duration(milliseconds: 50),
+          reverseDuration: const Duration(milliseconds: 200),
+          child: controller.value.isPlaying
+              ? const SizedBox.shrink()
+              : Container(
+                  color: Colors.black26,
+                  child: const Center(
+                    child: Icon(
+                      Icons.play_arrow,
+                      color: Colors.white,
+                      size: 100.0,
+                    ),
+                  ),
+                ),
+        ),
+        GestureDetector(
+          onTap: () {
+            if (controller.value.isPlaying) {
+              controller.pause();
+            } else {
+              controller.play();
+            }
+          },
+        ),
+      ],
+    );
+  }
+}
